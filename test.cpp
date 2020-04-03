baa5eebba6515bc9a0e23b47f1ed361bf0bcc916
Juan Cruz <juan.mdq22@gmail.com>
2020-04-03 18:32:46 -0300

    first commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+	std::string a;
+	std::cout << "introduce texto:";
+	std::cin >> a;
+
+	std::cout << "mostrando texto:" << std::endl;
+	std::cout << a << std::endl; 
+
+
+}
